4a392b39d4e62648405d9f76ca612390d3698be0
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-09-15 22:20:41 +0530

    hashing and salting added.

--- r-notes/Backend/routes/auth.js
+++ r-notes/Backend/routes/auth.js
@@ -1,12 +1,52 @@
 const express = require('express');
-const ruoter = express.Router();
+const router = express.Router();
 
 const User = require("../models/Users");
 const { body, validationResult } = require("express-validator");
-
-ruoter.get('/', (req, res)=>{
-  res.json({a:"Arpit", b:"Developer"})
-})
+const bcrypt = require("bcryptjs");
 
 
-module.exports = ruoter;
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User already exists with this email" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const passwrd = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: passwrd
+      })
+
+      const data = {
+        user: { id: user.id },
+      };
+      const jwtData = jwt.sign(data, secret);
+      res.json({ jwtData });
+      // console.log(jwtData);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+
+module.exports = router;
