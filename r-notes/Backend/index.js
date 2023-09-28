08783aaca22f884db3e8f8db67977e8e79b4b67a
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-09-28 21:21:13 +0530

    security issues.

--- r-notes/Backend/index.js
+++ r-notes/Backend/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 var app = express()
 var cors = require('cors')
 const mongoSanitize = require('express-mongo-sanitize');
-
-
-app.use(cors())
-app.use(mongoSanitize());
-
+const helmet = require('helmet');
 const mongoConnect = require('./db');
+
+app.use(cors());
+app.use(mongoSanitize());
+app.use(helmet);
+app.use(express.json());
 mongoConnect();
 
 const port = 5000;
-app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth')); 
 app.use('/api/notes', require('./routes/notes')); 
